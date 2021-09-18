27598e669d9e01c31c45286ecc81e826f7710bda
Joseph Sumis <Jsumis@comcast.net>
2021-09-18 09:40:05 -0500

    removing nested folder

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,9 @@ export default function Contact() {
         <div className="contact-div">
             <h1>Contact</h1>
 
+            <h2>LinkedIn</h2>
+            <h2>https://www.linkedin.com/in/joseph-sumis-179aa87a/</h2>
+
             <form>
              <h2>Name</h2>
                 <input
